2c533488832f8d0590efdc217457aac52301b98f
shivamkchoudhary <45448915+shivamkchoudhary@users.noreply.github.com>
2019-05-05 21:54:55 +0530

    Update afk.py

--- IHbot/modules/afk.py
+++ IHbot/modules/afk.py
@@ -22,7 +22,7 @@ def afk(bot: Bot, update: Update):
         reason = ""
 
     sql.set_afk(update.effective_user.id, reason)
-    update.effective_message.reply_text("{} is going to Fap right now".format(update.effective_user.first_name))
+    update.effective_message.reply_text("{} is going to Enjoy real world".format(update.effective_user.first_name))
 
 
 @run_async
@@ -34,7 +34,7 @@ def no_longer_afk(bot: Bot, update: Update):
 
     res = sql.rm_afk(user.id)
     if res:
-        update.effective_message.reply_text("{} is back after Fapping so long".format(update.effective_user.first_name))
+        update.effective_message.reply_text("{} is back to virtual world".format(update.effective_user.first_name))
 
 
 @run_async
